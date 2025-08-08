ae2140ef09b633009dc18c670512f61f7a779dbd
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-08 14:07:16 +0200

    fixup! python3Packages.findpython: 0.6.3 -> 0.7.0

--- pkgs/development/python-modules/findpython/default.nix
+++ pkgs/development/python-modules/findpython/default.nix
@@ -9,6 +9,7 @@
 
   # runtime
   packaging,
+  platformdirs,
 
   # tests
   pytestCheckHook,
@@ -20,18 +21,19 @@ let
 in
 buildPythonPackage {
   inherit pname version;
-  format = "pyproject";
-
-  disabled = pythonOlder "3.7";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-izFkfHY1J3mjwaCAZpm2jmp73AtcLd2a8qB6DUDGc9w=";
   };
 
-  nativeBuildInputs = [ pdm-backend ];
+  build-system = [ pdm-backend ];
 
-  propagatedBuildInputs = [ packaging ];
+  dependencies = [
+    packaging
+    platformdirs
+  ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
